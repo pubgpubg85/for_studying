d1ce839aa5eaa9bd7d4466118e4afc333aa13404
Dmytro <pubuna8585@gmail.com>
2024-03-13 23:38:33 +0200

    registration

--- main_page/urls.py
+++ main_page/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import RegisterView
 
 app_name = 'main_page'
 urlpatterns = [
     path('', views.main_page, name='main_page'),
+    path('registrate', RegisterView.as_view(), name='registrate'),
 ]
--- main_page/views.py
+++ main_page/views.py
@@ -1,7 +1,21 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+
+from main_page.forms import RegisterForm
 
 
 @login_required
 def main_page(request):
     return render(request, 'main_page/index.html')
+
+
+class RegisterView(FormView):
+    form_class = RegisterForm
+    template_name = 'registration/registration.html'
+    success_url = reverse_lazy('main_page:main_page')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
