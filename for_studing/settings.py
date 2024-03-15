9a4996f34fe6396f37edf54e91c7b923ce898a9a
Dmytro <pubuna8585@gmail.com>
2024-03-15 21:43:30 +0200

    new bug(

--- for_studing/settings.py
+++ for_studing/settings.py
@@ -134,4 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pubgpubg3@gmail.com'
+EMAIL_HOST_USER = 'vojtkodmitro64@gmail.com'
+EMAIL_HOST_PASSWORD = 'Pubuna8585@'
