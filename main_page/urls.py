995279ddc7be05d92650c2b95ec2cae15d8ff915
Dmytro <pubuna8585@gmail.com>
2024-03-16 22:13:43 +0200

    added custom template for passford reset page

--- main_page/urls.py
+++ main_page/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.main_page, name='main_page'),
     path('registrate', RegisterView.as_view(), name='registrate'),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password/reset_password.html"), name='reset_password'),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
