230b870b848e55a585b477a703e0d07d70291973
Dmytro <pubuna8585@gmail.com>
2024-03-21 19:50:13 +0200

    password reset done

--- main_page/urls.py
+++ main_page/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
          name="password_reset_confirm"),
 
     path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_done.html"),
+         auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"),
          name="password_reset_complete"),
 ]
